2cc2979226371583d3ae81391bcfb87daf011980
Deivyth Mena <driviti@hotmail.es>
2022-04-01 11:20:43 +0200

    Add logger in differents class

--- pets/src/main/java/com/kreitek/pets/PetApp.java
+++ pets/src/main/java/com/kreitek/pets/PetApp.java
@@ -3,16 +3,20 @@ package com.kreitek.pets;
 import com.kreitek.pets.controllers.CatController;
 import com.kreitek.pets.controllers.ControllerFactory;
 import com.kreitek.pets.controllers.DogController;
+import com.kreitek.pets.domain.Logger;
+
 import java.util.Scanner;
 
 public class PetApp {
 
     // TODO Logger declaration
+    Logger logger = new Logger();
 
     public static void main (String[] args) {
         ControllerFactory controllerFactory = new ControllerFactory();
         boolean end = false;
         System.out.println("Pet app has been initiated"); // TODO Logger
+        System.out.println("Logger");
         while (!end) {
             String command = waitForNewCommand();
             String[] commandArgs = command.split(":");
@@ -59,6 +63,7 @@ public class PetApp {
             }
         }
         System.out.println("Pet app has been ended"); // TODO Logger
+        System.out.println("Logger");
     }
 
     private static String getGetParam(String[] commandArgs) throws BadCommandException {
